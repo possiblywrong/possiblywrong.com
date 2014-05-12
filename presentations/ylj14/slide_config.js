4c097b483983f5086142fc676c0bdcfbf1b28644
Declan Conlon <dec@cantab.net>
2014-05-12 15:01:28 +1000

    Blindly adding my lambda jam slides

---
+++ presentations/ylj14/slide_config.js
@@ -0,0 +1,39 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Quicker Sort?<br>',
+    subtitle: 'Implementing generic linear time sorting<br><br><br>',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/lambda_jam.jpeg',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Declan Conlon',
+    company: 'Principle Engineer, &nbsp;Juniper Networks',
+    twitter: '@possiblywrong',
+    www: 'http://possiblywrong.com',
+    github: 'http://github.com/possiblywrong/ylj14'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
